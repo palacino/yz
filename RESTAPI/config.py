4cc11c63863586b1c0358b4aa670e039b53e6673
Lieven Verbeke <lieven.verbeke@gmail.com>
2019-05-06 17:16:08 +0200

    before refactoring

--- RESTAPI/config.py
+++ RESTAPI/config.py
@@ -1,4 +1,4 @@
 SECURITY_FOLDER = '/Users/lverbeke/Documents/software/security'
 REFERENCE_DATA_PATH = '../data/anomaly.csv'
 CLEAN_REFERENCE_DATA_PATH = '../data/clean_data.csv'
-KNN_SEARCH_TYPE = 'brute-force'
+KNN_SEARCH_TYPE = 'mykdtree'
